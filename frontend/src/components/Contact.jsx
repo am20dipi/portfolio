0566142c13d47f2f0d12bffdd4a61104113cd8f8
Adriana <am20dipi@alum.siena.edu>
2022-03-03 21:36:18 -0500

    Building out Contact component with form

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,26 +12,29 @@ function Contact(){
                             {/* linkedin */}
                             <div className="c-info-item">
                                 <a href="https://www.linkedin.com/in/adriana-dipietro/" className="c-icon" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+                               {/*  /adriana-dipietro */}
                             </div>
                            {/*  email */}
                             <div className="c-info-item">
                                 <a href="https://mail.google.com/mail/?view=cm&fs=1&to=adrianadipietro4@gmail.com&su=Hi+Adriana" className="c-icon" target="_blank" rel="noopener noreferrer"><i className="fa fa-google"></i></a>
+                                {/* adrianadipietro4@gmail.com */}
                             </div>
                             {/* github */}
                             <div className="c-info-item">
-                            <a href="https://github.com/am20dipi" target="_blank" className="c-icon" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+                                <a href="https://github.com/am20dipi" target="_blank" className="c-icon" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+                                {/* /am20dipi */}
                             </div>
                         </div>
                     </div>
                     <div className="c-right">
-                        <div className="c-description">
+                        <p className="c-description">
                             <b>Questions?</b> Get in touch. 
-                        </div>
-                        <form>
+                        </p>
+                        <form className="contact-form">
                             <input type="text" placeholder="name" name="name" />
                             <input type="email" placeholder="email" name="email"/>
                             <textarea rows="5" placeholder="type your message here" name="message"></textarea>
-                            <button>submit</button>
+                            <button className="submit">submit</button>
                         </form>
                     </div>
                 </div>
